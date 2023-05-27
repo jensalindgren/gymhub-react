f1850e22d001e9b5f9d071e931200302ec7acaff
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-05-27 19:09:41 +0000

    See if this solves the problem

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
+  <React.StrictMode>
     <Router>
       <App />
-    </Router>,
+    </Router>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
