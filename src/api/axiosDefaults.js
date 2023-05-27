c37c61b4f5f051ebf113716940cfe60575c3bd82
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-05-27 23:25:38 +0000

    see if this works

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,9 +3,10 @@ import axios from "axios";
 
 // Axios default values
 axios.defaults.baseURL = "https://drf-api-backend.herokuapp.com/";
-axios.defaults.headers.post["Content-Type"] = "multi-part/form-data";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 // Axios request with the above default custom values
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
+
