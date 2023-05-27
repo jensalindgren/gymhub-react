7bc0786a02dd9d64f3163682f65473f48abcca3b
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-05-27 21:00:26 +0000

    See if this solves the problem

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // Axios default values
-axios.defaults.baseURL = "https://drf-api-backend.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-api-backend.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multi-part/form-data";
 axios.defaults.withCredentials = true;
 
--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -7,7 +7,12 @@ import axios from "axios";
 import styles from "../../styles/SignUp.module.css";
 import btnStyles from "../../styles/Button.module.css";
 // Components
-import { Form, Button, Col, Row, Container, Alert } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
 // Notifications
 import { NotificationManager } from "react-notifications";
 
