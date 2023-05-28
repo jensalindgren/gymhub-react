bce8899eb73dfa2997012b27d286ebad93a546fb
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-05-28 00:08:51 +0000

    Added a sign in page and a current user context Implemented sign in functionality.

--- src/App.js
+++ src/App.js
@@ -6,28 +6,56 @@ import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import SignUp from "./pages/auth/SignUp";
+import SignIn from "./pages/auth/SignIn";
 import Container from "react-bootstrap/Container";
 // API
 import './api/axiosDefaults';
 // Notifications
 import { NotificationContainer } from "react-notifications";
 import "react-notifications/lib/notifications.css";
+import axios from "axios";
+
+import { createContext, useEffect, useState } from "react";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
   return (
-    <div className={styles.App}>
+  <CurrentUserContext.Provider value={currentUser}>
+  <SetCurrentUserContext.Provider value={setCurrentUser}>
+  <div className={styles.App}>
       <NavBar />
       <NotificationContainer />
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signin" render={() => <SignIn/>} />
           <Route exact path="/signup" render={() => <SignUp/>} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
       <Footer />
     </div>
+    </SetCurrentUserContext.Provider>
+  </CurrentUserContext.Provider>
+
   );
 }
 
