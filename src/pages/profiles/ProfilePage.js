66b33733a2164c7e49b1cab766a1e143f9080497
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-06-09 10:47:23 +0000

    Adding tokens to the local storage

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -67,7 +67,7 @@ function ProfilePage() {
           <Image
             className={styles.ProfileImage}
             roundedCircle
-            src={profile?.profile_image}
+            src={profile?.image}
           />
         </Col>
         <Col lg={6}>
