4ed2d8e63c2241c6aa882ad5672503dd607d5933
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-06-12 05:26:18 +0000

    Just a simple redirect to the new page.

--- src/pages/gymhub/GymHubPage.js
+++ src/pages/gymhub/GymHubPage.js
@@ -1,5 +1,6 @@
 // React and Router
 import { Link, } from "react-router-dom";
+import { useRedirect } from "../../hooks/useRedirect";
 // Components
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -17,6 +18,9 @@ import btnStyles from "../../styles/Button.module.css";
  */
  
 const GymHubPage = () => {
+
+    // Call the useRedirect hook
+    useRedirect("loggedIn");
 
     return (
 
