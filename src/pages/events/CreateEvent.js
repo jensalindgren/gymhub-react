4c97c9d22a157041df7fed8651da37e2e3257a82
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-06-12 09:18:19 +0000

    It works now I can create events and get them from my database

--- src/pages/events/CreateEvent.js
+++ src/pages/events/CreateEvent.js
@@ -1,88 +1,258 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import { axiosInstance } from "../../api/axiosDefaults";
-import styles from "../../styles/CreateEvent.module.css";
+// import React, { useState } from "react";
+// import { useHistory } from "react-router-dom";
+// import Button from "react-bootstrap/Button";
+// import Form from "react-bootstrap/Form";
+// import { axiosInstance } from "../../api/axiosDefaults";
+// import styles from "../../styles/CreateEvent.module.css";
 
-const CreateEvent = () => {
-    const [formData, setFormData] = useState({
-      title: "",
-      content: "",
-      image: "",
+// const CreateEvent = () => {
+//     const [eventData, setFormData] = useState({
+//       title: "",
+//       content: "",
+//       image: "",
+//     });
+//     const history = useHistory();
+  
+//     const handleChange = (e) => {
+//       if (e.target.name === "image") {
+//         setFormData({ ...eventData, [e.target.name]: e.target.files[0] });
+//       } else {
+//         setFormData({ ...eventData, [e.target.name]: e.target.value });
+//       }
+//     };
+  
+//     const handleSubmit = async (e) => {
+//       e.preventDefault();
+//       try {
+//         const { title, content, image } = eventData;
+  
+//         // Create form data object
+//         const data = new FormData();
+//         data.append("title", title);
+//         data.append("content", content);
+//         if (image) {
+//           data.append("image", image);
+//         }
+  
+//         await axiosInstance.post("/events/", data);
+//         history.push("/events");
+//       } catch (err) {
+//         // Handle create event error
+//       }
+//     };
+  
+//     return (
+//       <div className={styles.CreateEvent}>
+//         <h2>Create Event</h2>
+//         <Form onSubmit={handleSubmit}>
+//           <Form.Group controlId="title">
+//             <Form.Label>Title</Form.Label>
+//             <Form.Control
+//               type="text"
+//               placeholder="Enter event title"
+//               name="title"
+//               value={eventData.title}
+//               onChange={handleChange}
+//               required
+//             />
+//           </Form.Group>
+  
+//           <Form.Group controlId="content">
+//             <Form.Label>Content</Form.Label>
+//             <Form.Control
+//               as="textarea"
+//               rows={5}
+//               placeholder="Enter event content"
+//               name="content"
+//               value={eventData.content}
+//               onChange={handleChange}
+//             />
+//           </Form.Group>
+  
+//           <Form.Group controlId="image">
+//             <Form.Label>Image</Form.Label>
+//             <Form.Control
+//               type="file"
+//               accept="image/*"
+//               name="image"
+//               onChange={handleChange}
+//             />
+//           </Form.Group>
+  
+//           <Button type="submit">Create</Button>
+//         </Form>
+//       </div>
+//     );
+//   };
+  
+//   export default CreateEvent;
+
+import React, { useRef, useState } from "react";
+import { useHistory } from "react-router";
+import { axiosInstance } from "../../api/axiosDefaults";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+
+import Asset from "../../components/Asset";
+import Upload from "../../assets/upload.png";
+
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+const CreateEvents = () => {
+  const [errors, setErrors] = useState({});
+
+  const [eventData, setEventData] = useState({
+    title: "",
+    content: "",
+    image: "",
+  });
+
+  const { title, content, image } = eventData;
+  const imageInput = useRef(null);
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setEventData({
+      ...eventData,
+      [event.target.name]: event.target.value,
     });
-    const history = useHistory();
-  
-    const handleChange = (e) => {
-      if (e.target.name === "image") {
-        setFormData({ ...formData, [e.target.name]: e.target.files[0] });
-      } else {
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-      }
-    };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-        const { title, content, image } = formData;
-  
-        // Create form data object
-        const data = new FormData();
-        data.append("title", title);
-        data.append("content", content);
-        if (image) {
-          data.append("image", image);
-        }
-  
-        await axiosInstance.post("/events/", data);
-        history.push("/events");
-      } catch (err) {
-        // Handle create event error
-      }
-    };
-  
-    return (
-      <div className={styles.CreateEvent}>
-        <h2>Create Event</h2>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group controlId="title">
-            <Form.Label>Title</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter event title"
-              name="title"
-              value={formData.title}
-              onChange={handleChange}
-              required
-            />
-          </Form.Group>
-  
-          <Form.Group controlId="content">
-            <Form.Label>Content</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={5}
-              placeholder="Enter event content"
-              name="content"
-              value={formData.content}
-              onChange={handleChange}
-            />
-          </Form.Group>
-  
-          <Form.Group controlId="image">
-            <Form.Label>Image</Form.Label>
-            <Form.Control
-              type="file"
-              accept="image/*"
-              name="image"
-              onChange={handleChange}
-            />
-          </Form.Group>
-  
-          <Button type="submit">Create</Button>
-        </Form>
-      </div>
-    );
   };
-  
-  export default CreateEvent;
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setEventData({
+        ...eventData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("content", content);
+    formData.append("image", imageInput.current.files[0]);
+
+    try {
+      const { data } = await axiosInstance.post("/events/", formData);
+      history.push(`/events/${data.id}`);
+    } catch (error) {
+      if (error.response?.status !== 401) {
+        setErrors(error.response?.data);
+      }
+    }
+  };
+
+  const renderTextFields = (
+    <div className="text-center">
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.title?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Form.Group>
+        <Form.Label>Content</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="content"
+          value={content}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Blue}`}
+        onClick={() => history.goBack()}
+      >
+        Cancel
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+        Create
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+              {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className="d-flex justify-content-center"
+                  htmlFor="image-upload"
+                >
+                  <Asset
+                    src={Upload}
+                    message="Click or tap to upload an image"
+                  />
+                </Form.Label>
+              )}
+
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
+            </Form.Group>
+            {errors?.image?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <div className="d-md-none">{renderTextFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{renderTextFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default CreateEvents;
--- src/pages/events/PostEvent.js
+++ src/pages/events/PostEvent.js
@@ -1,50 +1,50 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
-import { axiosInstance } from "../../api/axiosDefaults";
-import appStyles from "../../App.module.css";
-import { useParams } from "react-router";
-
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 
-const EventPage = () => {
+import appStyles from "../../App.module.css";
+import { useParams } from "react-router";
+import { axiosInstance } from "../../api/axiosDefaults";
+import Events from "./Events";
+import PopularProfiles from "../profiles/PopularProfiles";
+
+function PostEvent() {
   const { id } = useParams();
-  const [event, setEvent] = useState(null);
+  const [event, setEvent] = useState({});
 
   useEffect(() => {
     const fetchEvent = async () => {
       try {
-        const response = await axiosInstance.get(`/events/${id}/`);
-        setEvent(response.data);
-      } catch (error) {
-        // Handle error
+        const { data } = await axiosInstance.get(`/events/${id}`);
+        setEvent(data);
+      } catch (err) {
+        console.log(err);
       }
     };
 
     fetchEvent();
   }, [id]);
 
-  console.log(event);
-
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
-        {event ? (
-          <Container className={appStyles.Content}>
-            <h3>{event.title}</h3>
-            <p>{event.content}</p>
-            <img src={event.image} alt={event.title} />
-          </Container>
-        ) : (
-          <p>Loading event...</p>
-        )}
+        <PopularProfiles mobile />
+        <Container className={appStyles.Content}>
+          <h2>Event Details</h2>
+          <p>Title: {event.title}</p>
+          <p>Content: {event.content}</p>
+          {event.image && (
+            <img src={event.image} alt="Event" className="img-fluid" />
+          )}
+          {/* Add more details as needed */}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        <PopularProfiles />
       </Col>
     </Row>
   );
-};
+}
 
-export default EventPage;
+export default PostEvent;
